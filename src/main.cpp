b964e75bdbbc5c1609d454629ae0a479df29eb92
yanis <yanis.auer@epitech.eu>
2020-03-22 16:10:01 +0100

    modif main

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,24 @@
 #include <dlfcn.h>
 #include <stdio.h>
 
-
 int main() {
     std::cout << "C++ dlopen demo\n\n";
 
     // open the library
     std::cout << "Opening hello.so...\n";
-    void* handle = dlopen("./hello.so", RTLD_LAZY);
+    void* handle = dlopen("./lib_arcade_sfml.so", RTLD_LAZY);
     if (!handle) {
         std::cerr << "Cannot open library: " << dlerror() << std::endl;
         return 1;
     }
 
- // load the symbol
+    // load the symbol
     std::cout << "Loading symbol hello...\n";
-    typedef void (*hello_t)();
 
- // reset errors
+    // reset errors
     dlerror();
-    hello_t hello = (hello_t) dlsym(handle, "hello");
+    void (*hello) ();
+    //hello = dlsym(handle, "hello");
     const char *dlsym_error = dlerror();
     if (dlsym_error) {
         std::cerr << "Cannot load symbol 'hello': " << dlsym_error << std::endl;
@@ -28,11 +27,11 @@ int main() {
         return 1;
     }
 
- // use it to do the calculation
+    // use it to do the calculation
     std::cout << "Calling hello...\n";
     hello();
 
- // close the library
+    // close the library
     std::cout << "Closing library...\n";
     dlclose(handle);
 }
