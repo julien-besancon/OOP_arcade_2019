eedba5734b9a2e0db1fa1cee7ec655e63d73e65f
Florian QUENE <florian.quene@epitech.eu>
2020-04-01 14:54:46 +0200

    [UPD] add menu and pause

--- lib/lib_arcade_ncurse.cpp
+++ lib/lib_arcade_ncurse.cpp
@@ -8,6 +8,7 @@
 #include "../src/Core.hpp"
 #include <ncurses.h>
 #include <curses.h>
+#include <string>
 
 class ncurse : public IGraph
 {
@@ -38,10 +39,12 @@ ncurse::~ncurse()
 
 void ncurse::init()
 {
+    initscr();
 }
 
 void ncurse::end()
 {
+    endwin();
 }
 
 void ncurse::display() 
@@ -69,17 +72,42 @@ char ncurse::get_input()
 }
 int menu_action()
 {
-    //getch();
+    int i = 1;
+    int command = getch();
 
-
-
-
-
-    return (0);
+    switch (command) {
+    case KEY_UP: i++;
+        break;
+    case KEY_DOWN: i--;
+        break; 
+    }
+    if (i < 1) 
+        i = 1;
+    if (i > 4)
+        i = 4;
+    switch (i) {
+    case 1:
+        mvprintw(20, 25, "-->");
+        break;
+    case 2:
+        mvprintw(40, 25, "-->");
+        break;
+    case 3:
+        mvprintw(60, 25, "-->");
+        break;
+    case 4:
+        mvprintw(80, 25, "-->");
+        break;
+    }
+    return (i);
 }
 
 void display_menu()
 {
+    mvprintw(20, 30, "RESUME");
+    mvprintw(40, 30, "NEXT GAME");
+    mvprintw(60, 30, "PREVIOUS GAME");
+    mvprintw(80, 30, "EXIT");
 
 }
 
@@ -93,18 +121,45 @@ char ncurse::menu()
 
 int pause_action()
 {
-    //getch();
+    int i = 1;
+    int command = getch();
 
-
-
-
-
-
-    return (0);
+    switch (command) {
+    case KEY_UP: i++;
+        break;
+    case KEY_DOWN: i--;
+        break; 
+    }
+    if (i < 1) 
+        i = 1;
+    if (i > 5)
+        i = 5;
+    switch (i) {
+    case 1:
+        mvprintw(20, 25, "-->");
+        break;
+    case 2:
+        mvprintw(40, 25, "-->");
+        break;
+    case 3:
+        mvprintw(60, 25, "-->");
+        break;
+    case 4:
+        mvprintw(80, 25, "-->");
+        break;
+    case 5:
+        mvprintw(100, 25, "-->");
+        break;
+    }
+    return (i);
 }
 void display_pause()
 {
-
+    mvprintw(20, 30, "RESUME");
+    mvprintw(40, 30, "RESTART");
+    mvprintw(60, 30, "BACK TO MENU");
+    mvprintw(80, 30, "NEXT LIB");
+    mvprintw(100, 30, "PREVIOUS LIB");
 }
 
 char ncurse::pause() 
