88ee5bcda23b37ce532dda35551b25c01132759a
Besançon Julien <bjulien@localhost.localdomain>
2020-04-04 20:57:13 +0200

    beaucoup de modification mais problème d'affichage dans la fenetre, un rectangle blanc de la meme taille que les images, à leurs places bien sur

--- lib/lib_arcade_sfml.cpp
+++ lib/lib_arcade_sfml.cpp
@@ -7,42 +7,48 @@
 #include "../src/Core.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
 
 class sfml : public IGraph
 {
     public:
         sfml();
-        virtual ~sfml();
+        ~sfml();
 
         void display(int game_map[20][40]);
         input get_input(input current);
         input menu(Core &core);
         input pause();
         void display_score(int score);
+        void display_game_name(std::string name);
         std::string game_over_screen();
         void end();
-
-        void mesfonction(){};
-    private:
-        sf::Texture _texturemenu;
+        
         sf::Sprite _spritemenu;
-        sf::Texture _texturepause;
         sf::Sprite _spritepause;
         sf::RenderWindow _window;
 };
 
 sfml::sfml()
 {
-    sf::RenderWindow _window(sf::VideoMode(800, 600), "My games");
-    if (!_texturemenu.loadFromFile("../games/ressources/menumoche.png")) {
+    _window.create(sf::VideoMode(1920, 1080), "My window");
+    sf::Texture texturemenu;
+    sf::Texture texturepause;
+
+
+    if (!texturemenu.loadFromFile("./lib/ressource/sfml/menu_bg.jpeg")) {
         std::cout << "ERREUR... le fichier png du MENU MOCHE n'est pas présent dans le répertoire" << std::endl;
+        end();
     }
 
-    if (!_texturepause.loadFromFile("../games/ressources/menupause.png")) {
+    if (!texturepause.loadFromFile("./lib/ressource/sfml/menu_bg.jpeg")) {
         std::cout << "ERREUR... le fichier png du MENU PAUSE n'est pas présent dans le répertoire" << std::endl;
+        end();
     }
-    _spritepause.setTexture(_texturepause);
-    _spritemenu.setTexture(_texturemenu);
+    _spritepause.setTexture(texturepause);
+    _spritemenu.setTexture(texturemenu);
 }
 
 sfml::~sfml()
@@ -51,6 +57,11 @@ sfml::~sfml()
 
 void sfml::end()
 {
+    _window.close();
+}
+
+void sfml::display(int game_map[20][40])
+{
 }
 
 void sfml::display_score(int score)
@@ -58,33 +69,34 @@ void sfml::display_score(int score)
 
 }
 
+void sfml::display_game_name(std::string name)
+{
+
+}
+
 std::string sfml::game_over_screen()
 {
-    
+    return("");
 }
 
-void sfml::display(int game_map[20][40])
+input sfml::get_input(input current)
 {
-    std::cout << "SFML" << std::endl;
-
-}
-
-input sfml::get_input()
-{
+    return(current);
 }
 
 input sfml::menu(Core &core)
-{
-    std::cout << "menu" << std::endl;
-    
-    _texturemenu.update(_window);
-    sf::RenderTexture.display()
+{   
+    _window.clear(sf::Color::Black);
+    _window.draw(_spritemenu);
+    _window.display();
+    return(undefinied);
 }
 
 input sfml::pause()
 {
-    _texturepause.update(_window);
-    _spritepause.
+    _window.draw(_spritepause);
+    _window.display();
+    return(undefinied);
 }
 
 extern "C" IGraph *create() {
